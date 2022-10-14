5a4109483fca22faf9cc021f8297b412cb5eca38
k1ll3rwh4le15 <n.solano.ns@gmail.com>
2022-10-14 11:14:25 -0400

    completed the functions to pass tests

--- index.js
+++ index.js
@@ -4,7 +4,10 @@
  * @param {Object} store - An object representing a single store. See the instructions for details on its shape.
  * @returns {Object[]} The same `stores` array that was inputted.
  */
-function addNewStore(stores, store) {}
+function addNewStore(stores, store) {
+  stores.push(store)
+  return stores
+}
 
 /**
  * Removes a store object at the given position.
@@ -12,14 +15,23 @@ function addNewStore(stores, store) {}
  * @param {number} index - A number representing the index of the store to be removed from the array.
  * @returns {Object[]} The same `stores` array that was inputted.
  */
-function removeStoreAtPosition(stores, index) {}
+function removeStoreAtPosition(stores, index) {
+  stores.splice (index,1)
+  return stores 
+}
 
 /**
  * Creates a duplicate of the `store` object. No references should be shared between the inputted `store` and the result.
  * @param {Object} store - An object representing a single store. See the instructions for details on its shape.
  * @returns {Object} The duplicated store object. This should not be the same as the store that was inputted.
  */
-function duplicateStore(store) {}
+function duplicateStore(store) {
+  let gameStore = {...store}
+  gameStore.boardGames = []
+  gameStore.boardGames.push(...store.boardGames)
+  gameStore.address = {...store.address}
+  return gameStore
+}
 
 module.exports = {
   addNewStore,
